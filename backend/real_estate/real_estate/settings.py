1425f359953393ff66601d492a3e8a061539ae19
michael <michaelngige@gmail.com>
2024-06-08 12:51:15 +0300

    django mail settings

--- backend/real_estate/real_estate/settings.py
+++ backend/real_estate/real_estate/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'realtors',
     'listings',
+    'contact',
 
     'corsheaders',
     'rest_framework',
@@ -93,6 +94,12 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mykalngigz@gmail.com'
+EMAIL_HOST_PASSWORD = 'ehpx rvko ilzn alzi'
+EMAIL_USE_TLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
