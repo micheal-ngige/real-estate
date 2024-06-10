c57a2add815e500d130f4e57588cfac6edd74825
michael <michaelngige@gmail.com>
2024-06-10 12:33:56 +0300

    scss styling

--- frontend/my-real-estate/src/store.jsx
+++ frontend/my-real-estate/src/store.jsx
@@ -1,16 +1,18 @@
-import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { composeWithDevTools } from "@redux-devtools/extension";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = configureStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: initialState,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleware),
+  devTools: composeWithDevTools(),
+});
 
 export default store;
